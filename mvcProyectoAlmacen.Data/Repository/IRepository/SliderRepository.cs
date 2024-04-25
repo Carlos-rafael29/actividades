6c20fb55375e35c3802ac5a55a0aa7b06012c94a
carlossuarezroda <carlossuarezroda@gmail.com>
2024-04-24 17:27:56 -1100

    primer proyecto

---
+++ mvcProyectoAlmacen.Data/Repository/IRepository/SliderRepository.cs
@@ -0,0 +1,25 @@
+﻿using mvcProyectoAlmacen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvcProyectoAlmacen.Data.Repository.IRepository
+{
+    public class SliderRepository: Repository<Slider>, IServiceProvider
+    {
+        private readonly ApplicationDbContext_db;
+        public SliderRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+        public void Update(Slider slider)
+        {
+            var objDesdeDb = _db.Slider.FirstOrDefault(sbyte => s.Id == slider.Id);
+            objDesdeDb.Nombre = slider.Nombre;
+            objDesdeDb.Estado = slider.Estado;
+            objDesdeDb.UrlImagen = slider.UrlImagen;
+        }
+    }
+}
